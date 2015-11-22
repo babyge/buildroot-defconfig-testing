87f6e0f3b24c5632c941935b7dabc51a0b1cbaa3
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-11-22 21:25:33 +0100

    Add script to update the Travis configuration
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ update.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# BUILDROOT_GIT=git://git.busybox.net/buildroot
+BUILDROOT_GIT=$HOME/projets/buildroot
+
+tmpdir=$(mktemp -d)
+git clone ${BUILDROOT_GIT}  ${tmpdir} || \
+    { rm -rf ${tmpdir}; exit 1; }
+
+# Get the latest commit
+commit=$(git --git-dir ${tmpdir}/.git show -s --format=%H) || \
+    { rm -rf ${tmpdir}; exit 1; }
+
+# Adjust .travis.yml to use the latest commit
+sed -i "s%^- git checkout.*%- git checkout ${commit}%" .travis.yml || \
+    { rm -rf ${tmpdir}; exit 1; }
+
+# Re-generate the list of defconfigs
+awk -i inplace -v gitrepo=${tmpdir} '
+/^  matrix:/ {
+  print;
+  system("for i in $(ls -1 " gitrepo "/configs); do echo \"   - DEFCONFIG_NAME=$i\" ; done");
+  inmatrix=1; }
+/^script:/ {
+  inmatrix=0;
+}
+// {
+  if (inmatrix) {
+    next;
+  }
+  print;
+}' .travis.yml ||
+    { rm -rf ${tmpdir}; exit 1; }
+
+# Commit
+git add .travis.yml
+git commit -s -m "Update to Buildroot ${commit}" || \
+    { rm -rf ${tmpdir}; exit 1; }
+
+git push || \
+    { rm -rf ${tmpdir}; exit 1; }
+
+rm -rf ${tmpdir}
